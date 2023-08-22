5220b7096b90ccb5688ebd039662ae1fbdda2de4
Md-Kais <mdkais3@gmail.com>
2023-08-22 10:29:32 +0600

    registration with firebase

---
+++ android/app/src/main/kotlin/com/kais29/retrieve_me/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kais29.retrieve_me
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:retrieve_me/Pages/registration.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Retrieve-Me',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+      useMaterial3: true,
+    ),
+    home: const RegistrationPage(),
+  ));
+}
